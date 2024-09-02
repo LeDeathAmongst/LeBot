4d15a6fbee8dcdc7ef5c6ca7babee12a24f23f7b
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-02 17:34:53 -0400

    Update _events.py

--- redbot/core/_events.py
+++ redbot/core/_events.py
@@ -358,7 +358,7 @@ def init_events(bot, cli_flags):
                     "https://media.moddb.com/images/downloads/1/199/198436/MOSHED-2020-2-20-22-48-16.gif",
                 ]
             )
-            support_server = "https://discord.gg/9f7WV6V8ud"
+            support_server = "https://discord.gg/wD7Njw5t2p"
             embed = discord.Embed(color=discord.Color.red())
             embed.set_image(url=url)
             if await ctx.bot.is_owner(ctx.author):
